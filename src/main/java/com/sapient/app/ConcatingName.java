479855472f5f178a05f18f748c214779463be5f1
Rashi Sabberwal <rassabbe@publicisgroupe.net>
2022-11-30 00:45:51 +0530

    my first commit

---
+++ src/main/java/com/sapient/app/ConcatingName.java
@@ -0,0 +1,14 @@
+package com.sapient.app;
+
+
+
+public class ConcatingName {
+
+	public static void main(String[] args) {
+		String first="Rashi";
+		String last="Sabberwal";
+		System.out.println("Hello" + " " + first + " " + last);
+
+	}
+
+}
